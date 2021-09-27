a9914da9ba072b59f484061018b41b067dddeca7
wooseok123 <59460718+wooseok123@users.noreply.github.com>
2021-09-27 23:45:53 +0900

    plus today's date on the menu bar

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-const navBar_barIcon = document.querySelector("nav");
+const navBar_barIcon = document.querySelector(".toggleBar");
+const navBar = document.querySelector("nav");
 const navBar_active = document.querySelector(".leftbar")
 const navBar_logout = document.querySelector(".logout");
 const menu__palette = document.querySelector(".menu__palette");
 const menu_dropdown = document.querySelector(".dropdown");
 
 navBar_barIcon.addEventListener("click",() => {
+  navBar.classList.toggle("visible");
   navBar_active.classList.toggle("visible");
   navBar_barIcon.classList.toggle("visible");
 })
-
+console.log(navBar_barIcon);
 menu_dropdown.addEventListener("click",() => {
   menu__palette.classList.toggle("visible");
   menu_dropdown.classList.toggle("dropUp");
